38f38f1b28617e7f843ff86aec42e1ed7696b6f5
JoaksCodes <125510235+JoaksCodes@users.noreply.github.com>
2023-06-28 15:38:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let llevaAnteojos = confirm("¿Llevas anteojos?");
+
+if (llevaAnteojos) {
+  alert("Vieja, esto esta construido bajo un cementerio Indio....ni a palos compro, chao");
+} else {
+  alert("Bueno, no veo que haya nada malo....compro!");
+}
